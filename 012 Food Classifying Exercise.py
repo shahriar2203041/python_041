e93f66a83c14c463f99d1ffb08889b1733023bf4
Shahriar Kamal <shahriar2203041@gmail.com>
2025-06-05 00:28:48 +0600

    Create 012 Food Classifying Exercise.py

---
+++ 012 Food Classifying Exercise.py
@@ -0,0 +1,7 @@
+fav=str(input("whats your favourite food?"))
+if fav=="apple" or fav=="grape":
+    print("fruit")
+elif fav=="bacon" or fav=="steak":
+    print("meat")
+elif fav=="dirt" or fav=="worm":
+    print("yuck")
