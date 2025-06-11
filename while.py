8e7f0ac28c76f58e91d4614b91487f2ee1534c41
Shahriar Kamal <shahriar2203041@gmail.com>
2025-06-11 19:30:52 +0600

    Update while.py

--- while.py
+++ while.py
@@ -38,3 +38,16 @@ while times < 11:
 # 		smileys += "\U0001f600"
 # 		count += 1
 # 	print(smileys)
+
+##3
+msg = input("Say Something: ")
+
+
+while msg != "stop copying me":
+	print(msg)
+	msg = input()
+print("UGH FINE YOU WIN, BROTHER!")
+
+# while msg != "stop copying me":
+# 	msg = input(f"{msg}\n")
+# print("UGH FINE YOU WIN, BROTHER!")
